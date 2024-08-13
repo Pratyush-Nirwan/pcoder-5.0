fdda5bb5855b0f5cdf397bc32c7af59b205d7ab8
Pratyush-Nirwan <pratyushnirwan123@gmail.com>
2024-08-13 17:51:37 +0530

    initial responsive code

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -11,22 +11,25 @@ const Profile = () => {
             <div id="dp-div" className='glass-nofade card-inner'>
                 <img src={DP} alt="" id='dp' />
             </div>
-            <h3 className='title'>Pratyush Nirwan</h3>
-            <h6 className='card-small margin-10px text' style={{ margin: '10px' }}>DEVELOPER + DESIGNER</h6>
-            <hr />
+            <div id='profile-text'>
 
-            <div className='contact-icons'>
-                <div onClick={() => { Href('https://www.instagram.com/pratyush_nirwan') }}>
-                    <AiFillInstagram size={25} />
-                </div>
-                <div>
-                    <AiFillMail size={25} />
-                </div>
-                <div>
-                    <AiFillLinkedin size={25} />
-                </div>
-                <div>
-                    <AiFillGithub size={25} />
+                <h3 className='title'>Pratyush Nirwan</h3>
+                <h6 className='card-small margin-10px text' id='dd' style={{ margin: '10px' }}>DEVELOPER + DESIGNER</h6>
+                <hr />
+
+                <div className='contact-icons'>
+                    <div onClick={() => { Href('https://www.instagram.com/pratyush_nirwan') }}>
+                        <AiFillInstagram size={25} />
+                    </div>
+                    <div>
+                        <AiFillMail size={25} />
+                    </div>
+                    <div>
+                        <AiFillLinkedin size={25} />
+                    </div>
+                    <div>
+                        <AiFillGithub size={25} />
+                    </div>
                 </div>
             </div>
 
